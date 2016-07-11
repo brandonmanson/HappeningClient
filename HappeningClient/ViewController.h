a122daa28c25b927e70541e7005d9ee30147f3a1
Brandon Manson <brandon.manson@detroitlabs.com>
2016-07-11 10:13:22 -0400

    Initial commit with XCode project

---
+++ HappeningClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HappeningClient
+//
+//  Created by Brandon Manson on 7/11/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
